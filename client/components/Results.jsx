aa7ee9ce2e8b1abc98a610ea6ca6bc1f23e880af
Cherise <cherisetan@live.com>
2019-12-04 20:03:23 +1300

    Display answers based on correct or incorrect response

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { getQuestions } from '../api/game'
 // import { getQuestions } from '../api/game'
 
 class Results extends React.Component {
@@ -9,19 +10,49 @@ class Results extends React.Component {
         }
     }
 
-    componentDidMount() {
+    // componentDidMount = () => {
+    //     if (this.props.playerResponses[0].correctAnswer == this.props.playerResponses[0].selectedAnswer){
+    //         this.setState({
+    //             correct: true
+    //         })
+    //     }
+    // }
 
-    }
+    // display question again
 
+    // is selected answer correct
+    // if true - display selected answer with a green border
+    // if false - display selected answer with a red border, and display correct answer in grey border
 
     render() {
+        let response = this.props.playerResponses[0]
         return (
             <div>
                 <h1>Results Component</h1>
+                {response != undefined && <div>
+                    <h2>{response.question}</h2>
+
+                    {response.correctAnswer == response.selectedAnswer ? 
+                    <div>
+                        <h3>Correct: {response.correctAnswer}</h3></div> : 
+                    <div>
+                        <h3>Incorrect: {response.selectedAnswer}</h3>
+                        <h3>Correct: {response.correctAnswer}</h3>
+                    </div>}
+
+                </div>}
 
             </div>
         )
     }
 }
 
-export default connect()(Results)
+function mapStateToProps(state) {
+    return {
+        teamName: state.teamName,
+        playerResponses: state.playerResponses
+
+    }
+}
+
+export default connect(mapStateToProps)(Results)
