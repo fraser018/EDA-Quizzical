6a4ccf5634c0d219cd109b335d63cae93b3f4755
student <student@eda-bc-04.localdomain>
2019-12-04 19:03:43 +1300

    initialize game end

---
+++ client/components/GameEnd.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+class GameEnd extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  playAgain = (e) => {
+    e.preventDefault()
+    this.props.dispatch({
+      type: 'START_GAME',
+    })
+  }
+
+  render(){
+    return(
+      <>
+      <h1>Congrats, you played our game and survived!</h1>
+      {/* <h2>{this.props.teamName} from redux</h2> */}
+      {/* <p>you got 7 out of 10 answers correct </p> from redux number of correct and false responses */}
+      <button onClick={this.playAgain}>Play again!!</button>
+      </>
+    )
+  }
+}
+
+export default connect()(GameEnd)
