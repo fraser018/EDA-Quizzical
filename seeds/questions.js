bfe013827805b3b9e2153ed48ff2efa7b3379389
Cherise <cherisetan@live.com>
2019-12-05 14:10:17 +1300

    Configure app for deployment to heroku

--- seeds/questions.js
+++ seeds/questions.js
@@ -5,9 +5,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('questions').insert([
-        {id: 1, question: 'rowValue1', correct_answer: '', incorrect_answer_1: '', incorrect_answer_2: '', incorrect_answer_3: '',},
-        {id: 2, question: 'rowValue2', correct_answer: '', incorrect_answer_1: '', incorrect_answer_2: '', incorrect_answer_3: '',},
-        {id: 3, question: 'rowValue3', correct_answer: '', incorrect_answer_1: '', incorrect_answer_2: '', incorrect_answer_3: '',}
+        //{id: 1, question: 'rowValue1', correct_answer: '', incorrect_answer_1: '', incorrect_answer_2: '', incorrect_answer_3: '',},
+       
       ]);
     });
 };
--- seeds/users.js
+++ seeds/users.js
@@ -5,9 +5,7 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, name: 'rowValue1', team: '', captain: true, date_created: ''},
-        {id: 2, name: 'rowValue2', team: '', captain: true, date_created: ''},
-        {id: 3, name: 'rowValue3', team: '', captain: true, date_created: ''}
+        //{id: 1, name: 'rowValue1', team: '', captain: true, date_created: ''},
       ]);
     });
 };
