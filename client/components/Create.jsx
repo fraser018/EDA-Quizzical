fff638bf23809d8c9af2cd578445a3781f5964a4
Fraser <f.callum@googmail.com>
2019-12-06 16:53:39 +1300

    improve ux for login

--- client/components/Create.jsx
+++ client/components/Create.jsx
@@ -22,8 +22,8 @@ class Create extends React.Component {
     return String.fromCharCode(65 + r);
   }
 
-  generateCode = (e) => {
-    e.preventDefault()
+  generateCode = () => {
+    // e.preventDefault()
     let prefix = new Array(2).fill().map(() => this.getRandomUppercaseChar()).join(""),
       integer = Math.floor((Math.random() * 999) * 7);
     let newCode = prefix + integer
@@ -76,28 +76,40 @@ class Create extends React.Component {
     })
   }
 
+  componentDidMount(){
+    this.generateCode()
+  }
+
   render() {
     return (
 
       <main>
         <section className='setup'>
           <h1 className='setup-gameTitle'>Quizzical</h1>
-          <h1 className='setup-create'>Create A Game</h1>
+          <h1 className='setup-create'>Game Created</h1>
+                <h1>Team Code:</h1>
+                <h1>{this.state.team}</h1>
+                <p>Give this code to your team</p>
+        </section>
+
+        <section className='setup'>
+          <p>Enter your player name below:</p>
+              <input name="captainName" onChange={this.handleChange} value={this.state.captainName}/>
+                
           <form>
             <div className='setup-btns'>
-              <input name="captainName" onChange={this.handleChange} value={this.state.captainName}/>
                 <section>
                   <div className='setup-btns__btn' onClick={this.createTeam}>
                     Create Team
                 </div>
                 </section>
-                <section>
-                  <div className='setup-btns__btn' onClick={this.generateCode}>
-                    Generate Team
+            </div>
+            <section>
+              <p>Or click below to join another team:</p>
+                  <div className='setup-btns__btn' onClick={this.joinTeam}>
+                    Join Team
                 </div>
                 </section>
-                <h1>{this.state.team}</h1>
-            </div>
               {this.state.message != '' && <h2>{this.state.message}</h2>}
           </form>
         </section>
