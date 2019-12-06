b617a5fe55be5f05d9cc4df1aa3c3f516b8cf6d1
Cherise <cherisetan@live.com>
2019-12-06 15:47:52 +1300

    Handle scoring when player does not submit an answer

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -5,105 +5,109 @@ import socket from '../api/socket'
 import ResultSplash from './ResultSplash'
 
 class Results extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
     }
+  }
 
-    componentDidMount(){
-        if(this.props.playerResponses[0]){
-            if (this.props.playerResponses[0].selectedAnswer == this.props.playerResponses[0].correctAnswer) {
-                socket.emit('score', { score: 1, teamName: this.props.teamName })
-            }
-            else {
-                socket.emit('score', { score: 0, teamName: this.props.teamName })
-            }
-        }
-        setTimeout(() => {
-            this.setState({
-                showResults: true
-            })
-        }, 2000)
+  componentDidMount() {
+    if (this.props.playerResponses[0]) {
+      if (this.props.playerResponses[0].selectedAnswer == this.props.playerResponses[0].correctAnswer) {
+        socket.emit('score', { score: 1, teamName: this.props.teamName })
+      }
+      else {
+        socket.emit('score', { score: 0, teamName: this.props.teamName })
+      }
+
     }
-
-    nextQuestion = (event) => {
-        event.preventDefault()
-        socket.emit('new question', { teamName: this.props.teamName, numOfPlayers: this.props.players.length })
+    else {
+      socket.emit('score', { score: 0, teamName: this.props.teamName })
     }
+    setTimeout(() => {
+      this.setState({
+        showResults: true
+      })
+    }, 2000)
+  }
 
-    endGame = (event) => {
-        event.preventDefault()
-        socket.emit('increment pages', this.props.teamName)
+  nextQuestion = (event) => {
+    event.preventDefault()
+    socket.emit('new question', { teamName: this.props.teamName, numOfPlayers: this.props.players.length })
+  }
+
+  endGame = (event) => {
+    event.preventDefault()
+    socket.emit('increment pages', this.props.teamName)
+  }
+
+  render() {
+    let response = this.props.playerResponses[0]
+
+    if (!this.state.showResults) {
+      return < ResultSplash />
     }
+    else {
+      return (
+        <div className='results'>
+          <h1 className='results-gameTitle'>Quizzical</h1>
 
-    render() {
-        let response = this.props.playerResponses[0]
+          {response != undefined ? (
+            <div>
+              <h2 className='results-question'>{response.question}</h2>
 
-        if (!this.state.showResults){
-            return < ResultSplash />
-        }
-        else {
-            return (
-              <div className='results'>
-                <h1 className='results-gameTitle'>Quizzical</h1>
-
-                {response != undefined ? (
-                  <div>
-                    <h2 className='results-question'>{response.question}</h2>
-
-                    {response.correctAnswer == response.selectedAnswer ? (
-                      <div className='results-answers'>
-                        <h3>Correct: {response.correctAnswer}</h3>
-                      </div>
-                    ) : (
-                      <div className='results-answers'>
-                        <h3>Incorrect: {response.selectedAnswer}</h3>
-                        <h3>Correct: {response.correctAnswer}</h3>
-                      </div>
-                    )}
-                  </div>
-                ) : (
-                  <div>
-                    <h3>Be quicker next time!</h3>
+              {response.correctAnswer == response.selectedAnswer ? (
+                <div className='results-answers'>
+                  <h3>Correct: {response.correctAnswer}</h3>
+                </div>
+              ) : (
+                  <div className='results-answers'>
+                    <h3>Incorrect: {response.selectedAnswer}</h3>
+                    <h3>Correct: {response.correctAnswer}</h3>
                   </div>
                 )}
-                  <div className='results-btns'>
+            </div>
+          ) : (
+              <div>
+                <h3>Be quicker next time!</h3>
+              </div>
+            )}
+          <div className='results-btns'>
 
-                {this.props.player.captain && this.props.roundCount < this.props.totalRounds &&(
-                    <div
-                      className='results-btns__btn'
-                      onClick={this.nextQuestion}
-                    >
-                      Next Question
+            {this.props.player.captain && this.props.roundCount < this.props.totalRounds && (
+              <div
+                className='results-btns__btn'
+                onClick={this.nextQuestion}
+              >
+                Next Question
                     </div>
-                )}
-                {this.props.player.captain && this.props.roundCount == this.props.totalRounds && (
-                    <div
-                      className='results-btns__btn'
-                      onClick={this.endGame}
-                    >
-                      End Game
+            )}
+            {this.props.player.captain && this.props.roundCount == this.props.totalRounds && (
+              <div
+                className='results-btns__btn'
+                onClick={this.endGame}
+              >
+                End Game
                   </div>
-                )}
-              </div>
-              </div>
+            )}
+          </div>
+        </div>
 
-            )
-            
-        }        
+      )
+
     }
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        teamName: state.teamName,
-        playerResponses: state.playerResponses,
-        player: state.player,
-        players: state.players,
-        roundCount: state.roundCount,
-        totalRounds: state.totalRounds
-    }
+  return {
+    teamName: state.teamName,
+    playerResponses: state.playerResponses,
+    player: state.player,
+    players: state.players,
+    roundCount: state.roundCount,
+    totalRounds: state.totalRounds
+  }
 }
 
 export default connect(mapStateToProps)(Results)
