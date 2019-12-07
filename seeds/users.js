f34cf1193ffe6f5e141d40f2fda84d9533efd563
Cherise <cherisetan@live.com>
2019-12-07 16:16:57 +1300

    Remove date from users table

--- seeds/users.js
+++ seeds/users.js
@@ -5,10 +5,10 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, name: 'ross', team: 'hello', captain: true, date_created: '2019-12-07', game_started: false},
-        {id: 2, name: 'kelly', team: 'world', captain: true, date_created: '2019-12-07', game_started: false},
-        {id: 3, name: 'han', team: 'hello', captain: false, date_created: '2019-12-07', game_started: false},
-        {id: 4, name: 'ollie', team: 'hello', captain: false, date_created: '2019-12-07', game_started: false},
+        {id: 1, name: 'ross', team: 'hello', captain: true, game_started: false},
+        {id: 2, name: 'kelly', team: 'world', captain: true, game_started: false},
+        {id: 3, name: 'han', team: 'hello', captain: false, game_started: false},
+        {id: 4, name: 'ollie', team: 'hello', captain: false, game_started: false},
       ]);
     });
 };
