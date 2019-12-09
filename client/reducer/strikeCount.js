8f93f830b7c143658c40b5792f2ee0bc3c58a676
Cassandre-Guinut <guinutcassandre@gmail.com>
2019-12-10 11:03:51 +1300

    remove console.log

--- client/reducer/strikeCount.js
+++ client/reducer/strikeCount.js
@@ -1,7 +1,6 @@
 const reducer = (state = 0, action) => {
   switch (action.type){
     case 'SAVE_STRIKE':{
-      console.log(action.strike)
       return (state + 1) * action.strike
     }
     default:
