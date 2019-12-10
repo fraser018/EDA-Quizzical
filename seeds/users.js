e97ddd70bd7eb4b27c6e905e96128b2dadd449db
Cherise <cherisetan@live.com>
2019-12-10 16:48:37 +1300

    Update ids

--- seeds/users.js
+++ seeds/users.js
@@ -8,7 +8,7 @@ exports.seed = function(knex) {
         {id: 99991, name: 'ross', team: 'hello', captain: true, game_started: false, socket_id:'000'},
         {id: 99992, name: 'kelly', team: 'world', captain: true, game_started: false, socket_id:'001'},
         {id: 99993, name: 'han', team: 'hello', captain: false, game_started: false, socket_id:'002'},
-        {id: 4, name: 'ollie', team: 'hello', captain: false, game_started: false, socket_id:'003'},
+        {id: 99994, name: 'ollie', team: 'hello', captain: false, game_started: false, socket_id:'003'},
       ]);
     });
 };
